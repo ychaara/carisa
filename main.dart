df7aac4e5d339beb9cff40dd3837b99ae1daf776
Ycaa <167733926+ychaara@users.noreply.github.com>
2024-11-28 08:28:18 +0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,178 +1,140 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter layout demo',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Destinasi Liburanmu'),
+          title: Text('Evaluasi Akhir'),
         ),
-        body: Column(
-          children: const [
-            ImageSection(),
-            TitleSection(
-              name: 'One Day Tour Bromo by DiCa.Travel',
-              location: 'Probolinggo, Jawa Timur, Indonesia',
-            ),
-            ButtonSection(),
-            TextSection(
-              description: 'Atur jadwalmu, siapkan kuda besimu,siapkan danamu dan mari nikmati tour bromo by DiCa.Travel!',
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ImageSection extends StatelessWidget {
-  const ImageSection({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Image.asset(
-      'assets/bromo.jpg',
-      width: double.infinity,
-      height: 240,
-      fit: BoxFit.cover,
-    );
-  }
-}
-
-class TitleSection extends StatelessWidget {
-  const TitleSection({
-    super.key,
-    required this.name,
-    required this.location,
-  });
-
-  final String name;
-  final String location;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    name,
-                    style: const TextStyle(
+        body: Row(
+          children: [
+            // Column on the left - konten kiri dengan ukuran tetap
+            Container(
+              width: 300, // Ukuran tetap untuk kolom kiri
+              padding: EdgeInsets.all(16.0),
+              color: const Color.fromARGB(255, 235, 186, 52),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Tanya sama Mang OO yuk!',
+                    style: TextStyle(
+                      fontSize: 24,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-                Text(
-                  location,
-                  style: TextStyle(
-                    color: Colors.grey[500],
+                  SizedBox(height: 16),
+                  Container(
+                    padding: EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: const Color.fromARGB(255, 117, 88, 8)),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Cireng',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          'makanan tradisional Indonesia yang berasal dari daerah Ciledug, Banten. Cireng terbuat dari tepung kanji atau tepung tapioka yang dicampur dengan air dan bahan-bahan lainnya, kemudian dibentuk menjadi bulatan kecil atau pipih.',
+                        ),
+                        //ini buat bintang sama review
+                        SizedBox(height: 16),
+                        Row(
+                          children: [
+                            Icon(Icons.star),
+                            Icon(Icons.star),
+                            Icon(Icons.star),
+                            Icon(Icons.star),
+                            Icon(Icons.star),
+                            SizedBox(width: 8),
+                            Text('1K Reviews'),
+                          ],
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          children: [
+                            Container(
+                              padding: EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                border: Border.all(color: const Color.fromARGB(255, 244, 76, 54)),
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              //ini icon 3 nya
+                              child: Column(
+                                children: [
+                                  Icon(Icons.kitchen, color: const Color.fromARGB(255, 244, 54, 54)),
+                                  SizedBox(height: 8),
+                                  Text('PREP'),
+                                  Text('15 min'),
+                                ],
+                              ),
+                            ),
+                            SizedBox(width: 16),
+                            Container(
+                              padding: EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                border: Border.all(color: const Color.fromARGB(255, 36, 170, 36)),
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              child: Column(
+                                children: [
+                                  Icon(Icons.timer, color: Colors.green),
+                                  SizedBox(height: 8),
+                                  Text('COOK'),
+                                  Text('45 min'),
+                                ],
+                              ),
+                            ),
+                            SizedBox(width: 16),
+                            Container(
+                              padding: EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                border: Border.all(color: const Color.fromARGB(255, 0, 68, 255)),
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              child: Column(
+                                children: [
+                                  Icon(Icons.restaurant, color: const Color.fromARGB(255, 0, 110, 255)),
+                                  SizedBox(height: 8),
+                                  Text('FEEDS:'),
+                                  Text('2-4'),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
+                ],
+              ),
+            ),
+            // foto
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(16.0),
+                child: Image.asset(
+                  'assets/cirenglg.jpg',
+                  fit: BoxFit.cover, 
+                  height: double.infinity, 
                 ),
-              ],
+              ),
             ),
-          ),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          const Text('41'),
-        ],
-      ),
-    );
-  }
-}
-
-class ButtonSection extends StatelessWidget {
-  const ButtonSection({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final Color color = Theme.of(context).primaryColor;
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        ButtonWithText(
-          color: color,
-          icon: Icons.call,
-          label: 'HUBUNGI',
+          ],
         ),
-        ButtonWithText(
-          color: color,
-          icon: Icons.near_me,
-          label: 'RUTE',
-        ),
-        ButtonWithText(
-          color: color,
-          icon: Icons.share,
-          label: 'BAGIKAN',
-        ),
-      ],
-    );
-  }
-}
-
-class ButtonWithText extends StatelessWidget {
-  const ButtonWithText({
-    super.key,
-    required this.color,
-    required this.icon,
-    required this.label,
-  });
-
-  final Color color;
-  final IconData icon;
-  final String label;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class TextSection extends StatelessWidget {
-  const TextSection({
-    super.key,
-    required this.description,
-  });
-
-  final String description;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(32),
-      child: Text(
-        description,
-        softWrap: true,
       ),
     );
   }
